8df9fc4cbd6448e2350b36605e9243eabae51e14
phamtuan11520011 <phamtuan1152001@gmail.com>
2024-03-14 17:47:46 +0700

    UPDATE DEPLOY API

--- src/config/api.js
+++ src/config/api.js
@@ -1,2 +1,3 @@
 export const BASE_URL_API_DEV = "http://localhost:3002"
+// export const BASE_URL_API_DEV = "https://ecommerce-ptuanstore-api.onrender.com"
 export const URL_DEV_VNAPPMOB = "https://vapi.vnappmob.com/api"
