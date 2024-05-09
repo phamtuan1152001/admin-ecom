d59f27165ee6a38bc52773d5aad72708c4933380
phamtuan11520011 <phamtuan1152001@gmail.com>
2024-05-09 18:24:20 +0700

    push notification to client

--- src/Pages/Order/service/api.js
+++ src/Pages/Order/service/api.js
@@ -2,3 +2,5 @@ export const GET_LIST_ORDERS_ADMIN = "/order/get-list-order-admin"
 export const GET_DETAIL_ORDER_ADMIN = "/order/get-detail-order-admin"
 export const UPDATE_DETAIL_ORDER_ADMIN = "/order/update-detail-order-admin"
 export const DELETE_DETAIL_ORDER_ADMIN = "/order/delete-detail-order-admin"
+
+export const SEND_NOTI_TO_CLIENT = "/notification/send-noti-client"
